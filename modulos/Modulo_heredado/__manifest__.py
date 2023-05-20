a9f2b7b8d1caf211100762a63aa3f74bf5b95e2b
jleon2058 <arscaballerie@gmail.com>
2023-05-20 13:51:06 -0500

    Se agrego texto cabecera a la descripcion

--- modulos/Modulo_heredado/__manifest__.py
+++ modulos/Modulo_heredado/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name':'Modulo de Venta Heredado',
-    'description':'Determinar las funcionalidades del modulo de Ventas Heredado agregar cabecera',
+    'description':'Determinar las funcionalidades del modulo de Ventas Heredado agregar cabecera es mi blog de cabecera',
     'depends':['sale',],
     'data':['views/sale_order_view.xml']
 }
