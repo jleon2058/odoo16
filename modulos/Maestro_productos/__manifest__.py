66dc2dc2441f9c4a0f7a4e9ad081f5e7e986cf5d
jleon2058 <arscaballerie@gmail.com>
2023-05-19 19:43:13 -0500

    se agrego nuevamente la palabra clonado

--- modulos/Maestro_productos/__manifest__.py
+++ modulos/Maestro_productos/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name':'Productos Tabla Maestra',
-    'description':'Determinar las funcionalidades del modulo de Productos Heredado',
+    'description':'Determinar las funcionalidades del modulo de Productos Heredado y clonado eterane',
     'depends':['product',],
     'data':['views/lista_productos.xml']
 }
