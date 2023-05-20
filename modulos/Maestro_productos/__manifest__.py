ffcac1f8cb5ba15ba56137c3f72d382a1bfbf66e
jleon2058 <arscaballerie@gmail.com>
2023-05-20 11:07:32 -0500

    Commit al master en su version mas reciente

--- modulos/Maestro_productos/__manifest__.py
+++ modulos/Maestro_productos/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name':'Productos Tabla Maestra',
-    'description':'Determinar las funcionalidades del modulo de Productos Heredado y clonado eterane',
+    'description':'Determinar las funcionalidades del modulo de Productos Heredado y clonado eterane , Agrega de lina',
     'depends':['product',],
     'data':['views/lista_productos.xml']
 }
