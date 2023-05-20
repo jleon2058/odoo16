83a9f350532ac8aa968cc956a8245c3d50d7e66c
jleon2058 <arscaballerie@gmail.com>
2023-05-19 19:19:05 -0500

    Se actualiza la lista

--- modulos/Maestro_productos/__manifest__.py
+++ modulos/Maestro_productos/__manifest__.py
@@ -1,6 +1,6 @@
 {
     'name':'Productos Tabla Maestra',
-    'description':'Determinar las funcionalidades del modulo de Productos Heredado',
+    'description':'Determinar las funcionalidades del modulo de Productos Heredado y Clonado',
     'depends':['product',],
     'data':['views/lista_productos.xml']
 }
--- modulos/Maestro_productos/models/product_template.py
+++ modulos/Maestro_productos/models/product_template.py
@@ -3,4 +3,4 @@ from odoo import api,fields,models
 class Product_Heredado(models.Model):
     _inherit="product.template"
     
-    status_product=fields.Selection([('Normal','normal'),('Baja rotacion','bajarotacion'),('alternativo','alternativo'),('perecible','perecible')])
+    status_product=fields.Selection([('Normal','normal'),('Baja rotacion','bajarotacion'),('alternativo','alternativo'),('perecible','perecible'),('delicado','delicado')])
